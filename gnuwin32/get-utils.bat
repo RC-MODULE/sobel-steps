67ab4a0f38c77f15a2f545a1c2d3ba1d06ab3809
mushkaev <mushkaev@module.ru>
2015-07-13 14:09:45 +0300

    get-utils

--- gnuwin32/get-utils.bat
+++ gnuwin32/get-utils.bat
@@ -1,6 +1,6 @@
 rem set http_proxy=http://username:password@proxy:80/
 
-wget http://downloads.sourceforge.net/project/gnuwin32/make/3.81/make-3.81.exe
+wget http://downloads.sourceforge.net/project/gnuwin32/make/3.81/make-3.81.exe || echo "ERROR: Modify 'http_proxy' variable in get-utils.bat" && pause
 wget http://downloads.sourceforge.net/project/gnuwin32/unzip/5.51-1/unzip-5.51-1.exe
 wget http://downloads.sourceforge.net/project/gnuwin32/unzip/5.51-1/unzip-5.51-1-bin.zip
 wget http://downloads.sourceforge.net/project/gnuwin32/which/2.20/which-2.20-bin.zip
