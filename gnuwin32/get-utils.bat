4fdc6ebb9bffc470204ef45418a313a8684030af
mushkaev <mushkaev@module.ru>
2015-07-13 16:48:22 +0300

    cmd vs sh

--- gnuwin32/get-utils.bat
+++ gnuwin32/get-utils.bat
@@ -1,9 +1,10 @@
 rem set http_proxy=http://username:password@proxy:80/
 
-wget http://downloads.sourceforge.net/project/gnuwin32/make/3.81/make-3.81.exe || echo "ERROR: Open /get-utils.bat and modify 'http_proxy' to your internet settings" && pause && exit
+wget http://downloads.sourceforge.net/project/gnuwin32/make/3.81/make-3.81.exe || @ echo "ERROR: Open ./get-utils.bat and modify 'http_proxy' with your valid internet settings" && exit /B
 wget http://downloads.sourceforge.net/project/gnuwin32/unzip/5.51-1/unzip-5.51-1.exe
 wget http://downloads.sourceforge.net/project/gnuwin32/unzip/5.51-1/unzip-5.51-1-bin.zip
 wget http://downloads.sourceforge.net/project/gnuwin32/which/2.20/which-2.20-bin.zip
 wget http://downloads.sourceforge.net/project/gnuwin32/coreutils/5.3.0/coreutils-5.3.0-bin.zip
+end
 
 
