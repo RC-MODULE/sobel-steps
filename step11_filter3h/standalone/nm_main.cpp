d2a30640a69fcfb15a955767dd5064e9e9932536
mushkaev <mushkaev@module.ru>
2015-07-23 18:41:19 +0300

    cfg ++

--- step11_filter3h/standalone/nm_main.cpp
+++ step11_filter3h/standalone/nm_main.cpp
@@ -3,10 +3,10 @@
 #include "nmplv.h"
 #include "easypgm.h"
 
-#pragma data_section ".data_source_pgm.bss"
-	long long src_pgm_file[1920*1080/8+8];
-#pragma data_section ".data_result_pgm.bss"
-	long long dst_pgm_file[1920*1080/8+8];
+#pragma data_section ".data_shared_src.bss"
+	long src_pgm_file[1920*1080/8+64/8];
+#pragma data_section ".data_shared_dst.bss"
+	long dst_pgm_file[1920*1080/8+64/8];
 	
 int main()
 {  
@@ -15,10 +15,8 @@ int main()
 
 	// parse pgm header	
 	int src_pgm_header_len=read_pgm_header(src_pgm_file,width,height);
-	if (src_pgm_header_len==0){
-		width=128;	// use default value if pgm-header is invalid
-		height=128;	// use default value if pgm-header is invalid
-	}
+	if (src_pgm_header_len==0) return -1;
+	
 	nm8u* src=VEC_Addr((nm8u*)src_pgm_file,src_pgm_header_len);
 	int dst_pgm_header_len=save_pgm_header(dst_pgm_file,width,height," Created By RC-Module(2015)");
 	nm8u* dst=VEC_Addr((nm8u*)dst_pgm_file,dst_pgm_header_len);
