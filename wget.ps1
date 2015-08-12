9e660c77f6037a68f0cbc32519d433f53cad57f0
mushkaev <mushkaev@module.ru>
2015-08-12 18:04:52 +0300

    proxy check

--- wget.ps1
+++ wget.ps1
@@ -1,12 +1,20 @@
 param ($url=$Args[0])
 $webclient = new-object System.Net.WebClient
-$proxy = new-object System.Net.WebProxy "proxy:80"
-$proxy.Credentials = New-Object System.Net.NetworkCredential ($env:USERNAME, $env:PASSWORD)
+$isDirect=$webClient.Proxy.IsBypassed("http://module.ru")
+if ($isDirect){}
+else {
+#$proxy = new-object System.Net.WebProxy "proxy:80"
+	netsh winhttp import proxy source=ie
+	$creds=Get-Credential
+	$webclient.Proxy.Credentials=$creds
+}
+#$proxy.Credentials = $creds
+#$proxy.Credentials = New-Object System.Net.NetworkCredential ($env:USERNAME, $env:PASSWORD)
 #$proxy = [System.Net.WebRequest]::GetSystemWebProxy()
 #$proxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials
 #$proxy.useDefaultCredentials = $true
 
-$webclient.proxy=$proxy
+#$webclient.proxy=$proxy
 $filename = [System.IO.Path]::GetFileName($url)
 $webclient.DownloadFile($url,$filename)
-$webclient.DownloadFile($url,$filename)
+#$webclient.DownloadFile( "http://savepic.ru/7691336.png","7691336.png")
