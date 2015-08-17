f9f806919120fb4ca0d04b01d111c902cee80489
mushkaev <mushkaev@PROTON.module.ru>
2015-08-17 23:03:43 +0300

    sss

--- environment.mk
+++ environment.mk
@@ -7,9 +7,9 @@ VSHELL32 = $(realpath $(ROOT))/deps/vshell32
 MC5103   = $(realpath $(ROOT))/deps/mc5103sdk
 MC7601   ?= $(realpath $(ROOT))/deps/mc7601sdk
 MB7707   = $(realpath $(ROOT))/deps/mb7707sdk
-MB7707_MAC ?= 1A-2B-3C-4D-5E-6F
 
-PATH    := $(NEURO)/bin;$(MC5103)/bin;$(MB7707)/bin;$(MC7601)/bin;$(ROOT)/gnuwin32/bin;$(VSHELL32)/bin;$(PATH)
+
+
   
 ifeq ($(OS),Windows_NT)
   SHELL    = cmd
@@ -21,13 +21,20 @@ ifeq ($(OS),Windows_NT)
  #OS_UNZIP = unzip 
   OS_UNZIP = powershell  -ExecutionPolicy Bypass -file $(ROOT)/unzip.ps1 
   OS_TODIR = -d
+  OS_TAR   = $(OS_UNZIP)
+  PATH    := $(NEURO)/bin;$(MC5103)/bin;$(MB7707)/bin;$(MC7601)/bin;$(ROOT)/gnuwin32/bin;$(VSHELL32)/bin;$(PATH)
+  MB7707_MAC ?= 1A-2B-3C-4D-5E-6F
 else
   OS_RM    = rm -f -r
   OS_RD    = rm -d -r
   OS_WHICH = which
   OS_WGET  = wget  
-  OS_UNZIP = tar xvzf 
+  OS_UNZIP = unzip
+  OS_TAR   = tar xvzf 
   OS_TODIR = -C
+  PATH    := $(NEURO)/bin:$(MC5103)/bin:$(MB7707)/bin:$(PATH)
+  LD_LIBRARY_PATH = $(MC5103)/bin:$(MB7707)/bin
+  MB7707_MAC = eth0
 endif
 
 
