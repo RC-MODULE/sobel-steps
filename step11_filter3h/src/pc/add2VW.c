2767e99d98355bf0749733cd907350f61069d234
mushkaev <mushkaev@module.ru>
2016-09-16 19:46:11 +0300

    add

---
+++ step11_filter3h/src/pc/add2VW.c
@@ -0,0 +1,8 @@
+#include "nmplv.h"
+void add2VW (nm16s *pSrcVec0,nm16s *pSrcVec1, nm16s *pSrcVec2, nm16s *pDstVec, int nSize) {
+	int i,x;
+	for(i=0;i<nSize;i++,pSrcVec0++,pSrcVec1++, pSrcVec2++ , pDstVec++){
+		x=(*pSrcVec0)*2+(*pSrcVec1)+(*pSrcVec2);
+		*pDstVec=x;
+	}
+}
