ea71a8554f1ad965c107bf50b6c6ab9e4615d940
Sergey Mushkaev <mushkaev@module.ru>
2018-04-12 15:55:33 +0300

    step

---
+++ step12_filter3v/make_virtual/premake5.lua
@@ -0,0 +1,42 @@
+#!lua
+
+
+-- A solution contains projects, and defines the available configurations
+solution "sobel-host"
+   configurations { "Debug", "Release" }
+
+   -- A project defines one build target
+   project "sobel-host"
+		kind "ConsoleApp"
+		language "C++"
+		files { "../src_host/*.cpp"}
+		links { "hal-virtual-x86.lib","vshell.lib" } 
+		includedirs { "../../../include","$(HAL)/include","$(VSHELL)/include"}	  
+		libdirs { "$(HAL)/lib","$(VSHELL)/lib"}
+
+		configuration "Debug"
+			defines { "DEBUG" }
+			symbols  "On" 
+
+		configuration "Release"
+			defines { "NDEBUG" }
+			symbols  "Off" 
+		 
+solution "sobel-target"
+    configurations { "Debug", "Release" }
+	 
+	project "sobel-target"
+		kind "ConsoleApp"
+		files { "../src_target/*.cpp","../src/common/*.cpp","../src/pc/*.cpp",}
+		links { "hal-virtual-x86.lib","nmpp-x86.lib" } 
+		includedirs { "$(HAL)/include","$(NMPP)/include","../src_common"}	  
+		libdirs { "$(HAL)/lib","$(NMPP)/lib"}
+		
+		configuration "Debug"
+			defines { "DEBUG" }
+			symbols  "On" 
+
+		configuration "Release"
+			defines { "NDEBUG" }
+			symbols  "Off" 
+		
